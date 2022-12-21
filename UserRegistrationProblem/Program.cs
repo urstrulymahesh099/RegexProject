0ae9d64177c6ee21611c23b643e40735f8510ed2
mago mahi <maheshhonnamurthy099@gmail.com>
2022-12-21 18:59:42 +0530

    [Mahesh]Add: Enetr Valid Last Name

--- UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/Program.cs
@@ -10,13 +10,17 @@ namespace RegexProject
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine(" 1.Enter Valid Name ");
+                Console.WriteLine(" 1.Enter Valid Name\n2.Enter Valid Last Name");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
                         UserRegistrationProblem name = new UserRegistrationProblem();
                         name.CheckName();
+                        break;
+                    case 2:
+                        UserRegistrationProblem lastname = new UserRegistrationProblem();
+                        lastname.CheckLastName();
                         break;
                     default:
                         flag = false;
--- UserRegistrationProblem/UserRegistrationProblem.cs
+++ UserRegistrationProblem/UserRegistrationProblem.cs
@@ -25,5 +25,21 @@ namespace RegexProject
                 Console.WriteLine(name + " is Invalid Name");
             }
         }
+        public void CheckLastName()
+        {
+            Console.WriteLine("Enter last name");
+            var name = Console.ReadLine();
+            string pattern = "[A-Z]{1}[a-z]{3,}";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(name))
+            {
+
+                Console.WriteLine(name + " is Valid Name");
+            }
+            else
+            {
+                Console.WriteLine(name + " is Invalid Name");
+            }
+        }
     }
 }
