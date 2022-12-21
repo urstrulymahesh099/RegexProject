73056de9232252bf1e4a5e2471aa7f9da900cf5e
mago mahi <maheshhonnamurthy099@gmail.com>
2022-12-21 18:52:15 +0530

    [Mahesh]Add: UserRegistrationProblem

--- UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace RegexProject
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to UserRegistrationProblem Problem Statements");
+            Console.WriteLine("Select Below Mention Options");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine(" 1.Enter Valid Name ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        UserRegistrationProblem name = new UserRegistrationProblem();
+                        name.CheckName();
+                        break;
+                    default:
+                        flag = false;
+                        Console.WriteLine("Try Again");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ UserRegistrationProblem/UserRegistrationProblem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexProject
+{
+    internal class UserRegistrationProblem
+    {
+        public void CheckName()
+        {
+            Console.WriteLine("Enter name");
+            var name = Console.ReadLine();
+            string pattern = "[A-Z]{1}[a-z]{3,}";
+            Regex regex = new Regex(pattern);
+            if(regex.IsMatch(name))
+            {
+                
+                Console.WriteLine(name+ " is Valid Name");
+            }
+            else
+            {
+                Console.WriteLine(name + " is Invalid Name");
+            }
+        }
+    }
+}
